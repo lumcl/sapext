fb6f5daefdbd91207e3700934061ea68b8d7513f
lum <ibmt20@hotmail.com>
2016-05-20 22:51:54 +0800

    MO

--- app/models/month_end.rb
+++ app/models/month_end.rb
@@ -9,7 +9,8 @@ class MonthEnd
 
     sql = "
       insert into tmplum.ke30x
-        select a.vtweg,a.perio,a.budat,a.kndnr,a.artnr,a.kaufn,a.kdpos,b.vgbel,b.vgpos,b.vbeln,b.posnr,b.charg,b.fkimg,b.vrkme,a.vvr10,
+        select a.vtweg,a.perio,a.budat,a.kndnr,a.artnr,a.kaufn,a.kdpos,b.vgbel,b.vgpos,b.vbeln,b.posnr,b.charg,
+              decode(b.shkzg,'X',b.fkimg*-1,b.fkimg)fkimg,b.vrkme,a.vvr10,
               b.arktx,c.budat mfgdt,c.aufnr,' ' rldat,a.rowid rid,a.paobjnr,a.pasubnr,a.belnr,a.gjahr
           from sapsr3.ce13058 a
             join sapsr3.vbrp b on b.mandt=a.mandt and b.vbeln=a.rbeln and b.posnr=a.rposn
@@ -20,8 +21,8 @@ class MonthEnd
     Db.connection.execute(sql)
 
     sql = "
-      insert into tmplum.aufkx
-        select a.aufnr,b.stat,b.chgnr,0 bomlv,' ' xstat, ' ' xdate, ' ' xtime
+      insert into tmplum.aufkx(aufnr,stat,chgnr,bomlv,xstat,xdate,xtime,remark)
+        select a.aufnr,b.stat,b.chgnr,0 bomlv,' ' xstat, ' ' xdate, ' ' xtime, ' ' remark
           from sapsr3.aufk a
             left join sapsr3.jest b on b.mandt=a.mandt and b.objnr=a.objnr and b.inact=' ' and b.stat in ('I0045','I0046')
             left join tmplum.aufkx d on d.aufnr=a.aufnr
